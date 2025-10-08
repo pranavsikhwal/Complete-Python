8695b71d5dbb279357c2a6f89805602f42336f9f
Pranav Sikhwal <pranavsikhwal@gmail.com>
2025-10-08 22:09:20 +0530

    Initial commit

---
+++ day-01/day -01 project.py
@@ -0,0 +1,8 @@
+#Band Name Generator
+print("Welcome to Band name generator ")
+name = input("What is the name of city you grew up in?\n ")
+print(name)
+pet_name = input("What's your pet's name ?\n")
+print(pet_name)
+print("Your band name could be " + name + " " +pet_name)
+
---
+++ day-01/day -01 task.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("My name is Pranav \nI am learning python")    #\n for next line
+ #print("Raj Sharma")    #Show indentation error
+print("Raj" + " Sharma")  #We can add strings using + in python
+input("What is your name?") #takes input from user
+print("Heyy" + input("What is your name?") +" !")
+
+name = input("Enter your name: ")      #variables in python
+print("Hello", name)
+name = input("Enter your name: ")
+print("Hello", name)
+print(len(name))        #gives length of the string
+
+
+
+
